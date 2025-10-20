69ae1213b0a40ec968cee4ff234bdcffc487fce5
Olivia Xu <olivia.xu@beaconfireinc.com>
2025-10-20 10:55:22 -0400

    Add files via upload

---
+++ dags/empty_workflow_example.py
@@ -0,0 +1,16 @@
+import datetime
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="basic_three_empty_tasks",
+    start_date=datetime.datetime(2025, 7, 1),
+    schedule="@daily",
+    catchup=False,
+    tags=["demo", "basic"],
+) as dag:
+    task_a = EmptyOperator(task_id="task_a")
+    task_b = EmptyOperator(task_id="task_b")
+    task_c = EmptyOperator(task_id="task_c")
+
+    task_a >> task_b >> task_c
