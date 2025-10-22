8800bb9c298bb2d792dfbef3f3b5c65274b5cf4e
Olivia Xu <olivia.xu@beaconfireinc.com>
2025-10-22 12:07:18 -0400

    Update test_connection.py

--- dags/test_connection.py
+++ dags/test_connection.py
@@ -1,12 +1,25 @@
-from airflow import DAG
-from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+import os
 from datetime import datetime
+
+from airflow import DAG
+from airflow.decorators import task
+
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.providers.snowflake.transfers.copy_into_snowflake import CopyFromExternalStageToSnowflakeOperator
+
+SNOWFLAKE_CONN_ID = 'snowflake_conn'
+SNOWFLAKE_DATABASE = 'AIRFLOW0928'
+SNOWFLAKE_SCHEMA = 'DEV'
+
+SNOWFLAKE_ROLE = 'DE_0928'
+SNOWFLAKE_WAREHOUSE = 'DE_0928_WH'
+SNOWFLAKE_STAGE = 'Olivia_stage'
 
 with DAG(
     dag_id="test_snowflake_conn",
     start_date=datetime(2025, 1, 1),
     schedule_interval=None,
-    catchup=False,
+    catchup=True,
 ) as dag:
 
     test_conn = SnowflakeOperator(
@@ -16,3 +29,7 @@ with DAG(
     )
 
     test_conn
+
+
+
+
