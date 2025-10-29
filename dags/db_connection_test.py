978dfde6a7d09062e040bd2c62d6989f04c31edd
Mengying Ju <angel_jmy@berkeley.edu>
2025-10-28 20:49:56 -0700

    testing connection

---
+++ dags/db_connection_test.py
@@ -0,0 +1,38 @@
+from airflow import DAG
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from datetime import datetime
+
+SNOWFLAKE_CONN_ID = "snowflake_conn"
+
+with DAG(
+    dag_id="test_snowflake_connection",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=["test", "snowflake"],
+) as dag:
+
+    test_conn = SnowflakeOperator(
+        task_id="test_connection",
+        snowflake_conn_id=SNOWFLAKE_CONN_ID,
+        sql="""
+        SELECT CURRENT_ACCOUNT() AS account,
+               CURRENT_ROLE() AS role,
+               CURRENT_DATABASE() AS database_name,
+               CURRENT_SCHEMA() AS schema_name,
+               CURRENT_WAREHOUSE() AS warehouse;
+        """,
+    )
+
+    test_table = SnowflakeOperator(
+        task_id="test_create_drop",
+        snowflake_conn_id=SNOWFLAKE_CONN_ID,
+        sql="""
+        CREATE OR REPLACE TABLE TEST_CONN_TABLE (id INT);
+        INSERT INTO TEST_CONN_TABLE VALUES (1);
+        SELECT COUNT(*) AS rowcount FROM TEST_CONN_TABLE;
+        DROP TABLE TEST_CONN_TABLE;
+        """,
+    )
+
+    test_conn >> test_table
